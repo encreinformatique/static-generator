586ee31ec096331dadd110b06fe6e1a7f22b19d8
Encre Informatique <encreinformatique@users.noreply.github.com>
2024-05-28 17:43:21 +0000

    arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-/usr/bin/generator
+/usr/bin/generator "" "${INPUT_HEADER}" "${INPUT_FOOTER}"
 npm run build
 
 git add -A && git commit -m "Update Website"
