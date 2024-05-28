9b59c7cdfbdd499010ba061cfec72d7ff465224e
Encre Informatique <encreinformatique@users.noreply.github.com>
2024-05-28 19:26:29 +0200

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -10,17 +10,18 @@ fi
 
 for file in content/*; do
     _fileName=$(basename "$file")
+    _destFile="./$_destination${_fileName/.php/.html}"
     # We add the header or an empty string in the file.
     # We could also and create the file.
     if [ "$_header" != "" ]; then
-        php -dxdebug.mode=off "$_header" > "./$_destination${_fileName/.php/.html}"
+        php -dxdebug.mode=off "$_header" > "$_destFile"
     else
-        php -dxdebug.mode=off "" > "./$_destination${_fileName/.php/.html}"
+        php -dxdebug.mode=off "" > "$_destFile"
     fi
 
-    php -dxdebug.mode=off "./$file" >> "./$_destination${_fileName/.php/.html}"
+    php -dxdebug.mode=off "./$file" >> "$_destFile"
 
     if [ "$_footer" != "" ]; then
-        php -dxdebug.mode=off "$_footer" >> "./$_destination${_fileName/.php/.html}"
+        php -dxdebug.mode=off "$_footer" >> "$_destFile"
     fi
 done
